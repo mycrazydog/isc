62382b4b629222b8590e32a9e3e3ee4e781418af
snipe <snipe@snipe.net>
2014-03-22 05:11:41 -0400

    Prettier sub-nav

--- app/views/frontend/layouts/account.blade.php
+++ app/views/frontend/layouts/account.blade.php
@@ -2,16 +2,16 @@
 
 {{-- Page content --}}
 @section('content')
+
 <div class="row">
-	<div class="span3">
-		<ul class="nav nav-list">
-			<li class="nav-header">Main Menu</li>
+
+		<ul class="nav nav-pills">
 			<li{{ Request::is('account/profile') ? ' class="active"' : '' }}><a href="{{ URL::route('profile') }}">Profile</a></li>
 			<li{{ Request::is('account/change-password') ? ' class="active"' : '' }}><a href="{{ URL::route('change-password') }}">Change Password</a></li>
 			<li{{ Request::is('account/change-email') ? ' class="active"' : '' }}><a href="{{ URL::route('change-email') }}">Change Email</a></li>
 		</ul>
-	</div>
-	<div class="span9">
+
+	<div class="col-sm-9">
 		@yield('account-content')
 	</div>
 </div>
