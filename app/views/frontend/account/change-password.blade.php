3eb496d8994169007a49e943d511e5e37dec5a46
snipe <snipe@snipe.net>
2014-03-24 00:09:43 -0400

    Internationalization

--- app/views/frontend/account/change-password.blade.php
+++ app/views/frontend/account/change-password.blade.php
@@ -18,7 +18,7 @@ Change your Password
 	<!-- Old Password -->
 	<!-- Confirm Password -->
 	<div class="form-group {{ $errors->first('old_password', 'has-error') }}">
-		<label for="old_password" class="col-sm-3 control-label">Old Password</label>
+		<label for="old_password" class="col-sm-3 control-label">@lang('account/form.oldpassword')</label>
 			<div class="col-sm-9">
 			<input type="password" id="old_password" name="old_password" class="form-control">
 			{{ $errors->first('old_password', '<span class="help-block">:message</span>') }}
@@ -27,7 +27,7 @@ Change your Password
 
 	<!-- New Password -->
 	<div class="form-group {{ $errors->first('password', 'has-error') }}">
-		<label for="password" class="col-sm-3 control-label">New Password</label>
+		<label for="password" class="col-sm-3 control-label">@lang('account/form.newpassword')</label>
 			<div class="col-sm-9">
 			<input type="password" id="password" name="password" class="form-control">
 			{{ $errors->first('password', '<span class="help-block">:message</span>') }}
@@ -36,7 +36,7 @@ Change your Password
 
 	<!-- Confirm New Password  -->
 	<div class="form-group {{ $errors->first('password_confirm', 'has-error') }}">
-		<label for="password_confirm" class="col-sm-3 control-label">New Password</label>
+		<label for="password_confirm" class="col-sm-3 control-label">@lang('account/form.confirmpassword')</label>
 			<div class="col-sm-9">
 			<input type="password" id="password_confirm" name="password_confirm" class="form-control">
 			{{ $errors->first('password_confirm', '<span class="help-block">:message</span>') }}
@@ -48,9 +48,9 @@ Change your Password
 	<!-- Form actions -->
 	<div class="control-group">
 		<div class="controls">
-			<button type="submit" class="btn btn-default">Update Password</button>
+			<button type="submit" class="btn btn-default">@lang('button.update')</button>
 
-			<a href="{{ route('forgot-password') }}" class="btn btn-link">I forgot my password</a>
+			<a href="{{ route('forgot-password') }}" class="btn btn-link">@lang('button.forgotpassword')</a>
 		</div>
 	</div>
 </form>
