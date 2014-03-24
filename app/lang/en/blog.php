9686bc9f65249d5ad165ca61e4b16dc78f759eb5
snipe <snipe@snipe.net>
2014-03-24 05:37:05 -0400

    New blog language file

---
+++ app/lang/en/blog.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Language file for blog public view
+*
+*/
+
+return array(
+
+	'addcomment'			=> 'Add a Comment',
+	'comment'				=> 'Comment',
+	'comments'				=> 'Comments',
+	'postcomment'			=> 'Post Comment',
+
+
+);
