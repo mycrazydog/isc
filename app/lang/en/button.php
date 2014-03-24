ed48adef3bbdad272564bed9eb19afd652c3f592
snipe <snipe@snipe.net>
2014-03-23 23:50:03 -0400

    More button labels

--- app/lang/en/button.php
+++ app/lang/en/button.php
@@ -2,8 +2,17 @@
 
 return array(
 
-	'edit'    => 'Edit',
-	'delete'  => 'Delete',
-	'restore' => 'Restore',
+	'edit'    			=> 'Edit',
+	'delete'  			=> 'Delete',
+	'restore' 			=> 'Restore',
+	'publish' 			=> 'Publish',
+	'save'	  			=> 'Save',
+	'cancel'  			=> 'Cancel',
+	'create'  			=> 'Create',
+	'back'    			=> 'Back',
+	'signin'  			=> 'Sign in',
+	'forgotpassword' 	=> 'I forgot my password',
+	'rememberme'		=> 'Remember me',
+	'signup'			=> 'Sign-Up',
 
 );
