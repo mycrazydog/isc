0425e124021984b008d9cc3ddb39d6d2cc733bb5
snipe <snipe@snipe.net>
2014-03-24 06:40:52 -0400

    Added home icon

--- app/views/frontend/layouts/default.blade.php
+++ app/views/frontend/layouts/default.blade.php
@@ -64,7 +64,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="/">Project name</a>
+          <a class="navbar-brand" href="/"><span class="glyphicon glyphicon-home"></span> Project name</a>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
