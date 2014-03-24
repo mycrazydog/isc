b93c41713a2fb01ef2356456958328ca711775fa
snipe <snipe@snipe.net>
2014-03-24 00:09:20 -0400

    New language files for account

---
+++ app/lang/en/account/form.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+
+	'password'				=> 'Password',
+	'email'					=> 'Email',
+	'newpassword'			=> 'New Password',
+	'website'				=> 'Website',
+	'country'				=> 'Country',
+	'gravataremail'			=> 'Gravatar Email',
+	'changegravatar'		=> 'Change your avatar at Gravatar.com',
+	'oldpassword'			=> 'Current Password',
+	'confirmpassword'		=> 'Confirm Password',
+
+);
---
+++ app/lang/en/auth/title.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+
+	'signin'				=> 'Sign In to Your Account',
+	'forgotpassword'		=> 'Forgot Your Password',
+	'signup'				=> 'Sign Up for an Account',
+
+);
