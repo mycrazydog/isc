7af077a202416c88e724a442be0d12822a6cc3d4
snipe <snipe@snipe.net>
2014-03-24 02:03:40 -0400

    User admin form text language file

---
+++ app/lang/en/admin/users/form.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* Language file for form fields for user account management
+*
+*/
+
+return array(
+
+	'password'				=> 'Password',
+	'email'					=> 'Email',
+	'newemail'				=> 'New Email',
+	'confirmemail'			=> 'Confirm Email',
+	'firstname'				=> 'First Name',
+	'lastname'				=> 'Last Name',
+	'newpassword'			=> 'New Password',
+	'website'				=> 'Website',
+	'country'				=> 'Country',
+	'gravataremail'			=> 'Gravatar Email',
+	'changegravatar'		=> 'Change your avatar at Gravatar.com',
+	'oldpassword'			=> 'Current Password',
+	'confirmpassword'		=> 'Confirm Password',
+	'activated'				=> 'Activated',
+	'groups'				=> 'Groups',
+	'grouphelp'				=> 'Select a group to assign to the user, remember that a user takes on the permissions of the group they are assigned.',
+
+);
