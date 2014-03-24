2cd21c1a5242c803b7d04b9c7c4c6f8ae0772ca6
snipe <snipe@snipe.net>
2014-03-24 05:37:24 -0400

    Cleanup of comments section, updated to Bootstrap 3

--- app/views/frontend/blog/view-post.blade.php
+++ app/views/frontend/blog/view-post.blade.php
@@ -37,26 +37,20 @@
 <hr />
 
 <a id="comments"></a>
-<h4>{{ $comments->count() }} Comments</h4>
+<h4>{{ $comments->count() }} @lang('blog.comments')</h4>
 
 @if ($comments->count())
 @foreach ($comments as $comment)
 <div class="row">
-	<div class="span1">
-		<img class="thumbnail" src="{{ $comment->author->gravatar() }}" alt="">
+	<div class="col-sm-1">
+		<img class="img-thumbnail pull-left" src="{{ $comment->author->gravatar() }}" alt="">
 	</div>
-	<div class="span11">
-		<div class="row">
-			<div class="span11">
+	<div class="col-sm-11">
 				<span class="muted">{{ $comment->author->fullName() }}</span>
 				&bull;
 				<span title="{{ $comment->created_at }}">{{ $comment->created_at->diffForHumans() }}</span>
+				<p>{{ $comment->content() }}</p>
 
-				<hr />
-
-				{{ $comment->content() }}
-			</div>
-		</div>
 	</div>
 </div>
 <hr />
@@ -69,23 +63,25 @@
 You need to be logged in to add comments.<br /><br />
 Click <a href="{{ route('signin') }}">here</a> to login into your account.
 @else
-<h4>Add a Comment</h4>
-<form method="post" action="{{ route('view-post', $post->slug) }}">
+<h4>@lang('blog.addcomment')</h4>
+<form class="form-horizontal" role="form" method="post" action="{{ route('view-post', $post->slug) }}">
+
 	<!-- CSRF Token -->
 	<input type="hidden" name="_token" value="{{ csrf_token() }}" />
 
 	<!-- Comment -->
-	<div class="control-group{{ $errors->first('comment', ' error') }}">
-		<textarea class="input-block-level" rows="4" name="comment" id="comment">{{ Input::old('comment') }}</textarea>
-		{{ $errors->first('comment', '<span class="help-inline">:message</span>') }}
-	</div>
-
-	<!-- Form actions -->
-	<div class="control-group">
-		<div class="controls">
-			<input type="submit" class="btn" id="submit" value="Submit" />
+	<div class="form-group {{ $errors->first('comment', 'has-error') }}">
+		<div class="col-sm-20">
+			{{ $errors->first('comment', '<span class="help-block">:message</span>') }}
+			<textarea rows="4" id="comment" name="comment" class="form-control" placeholder="Type your comment here"></textarea>
 		</div>
 	</div>
+	<div class="form-group">
+		<div class="col-sm-offset-10 col-sm-1">
+		  <button type="submit" class="btn btn-default">@lang('blog.postcomment')</button>
+		</div>
+  	</div>
+
 </form>
 @endif
 @stop
