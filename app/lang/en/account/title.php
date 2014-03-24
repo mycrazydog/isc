8b12313033a7b751b795f31f50a3302ff8fbb1bf
snipe <snipe@snipe.net>
2014-03-24 00:17:51 -0400

    New language file for account page titles

---
+++ app/lang/en/account/title.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Language file for page titles for user account management
+*
+*/
+
+return array(
+
+	'editprofile'				=> 'Edit Profile',
+	'changepasswordsubtitle'	=> 'Change Your Password',
+	'editprofilesubtitle'		=> 'Change Profile Details',
+	'changeemailsubtitle'		=> 'Update Your Email',
+
+);
