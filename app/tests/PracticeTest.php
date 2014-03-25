f56323713f4958ddbcd4bec248787d4ae89cdb4f
snipe <snipe@snipe.net>
2014-03-25 00:46:54 -0400

    Dummy tests - nothing useful here yet

---
+++ app/tests/PracticeTest.php
@@ -0,0 +1,15 @@
+<?php //lesson/tests/PracticeTest.php
+
+class PracticeTest extends PHPUnit_Framework_TestCase {
+	public function testHelloWorld()
+	{
+ 		$greeting = 'Hello, World.';
+ 		$this->assertTrue($greeting === 'Hello, World.');
+	}
+
+	public function testLaravelDevsIncludesDayle() {
+		$names = ['Taylor', 'Shawn', 'Dayle'];
+		$this->assertContains('Dayle', $names);
+	}
+
+}
