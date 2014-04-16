ba45cb6bbd2797c70fc0a14e7ad1c157c394bba3
snipe <snipe@snipe.net>
2014-04-16 02:33:51 -0400

    New translations

--- app/lang/en/blog.php
+++ app/lang/en/blog.php
@@ -10,5 +10,11 @@ return array(
     'comment'				=> 'Comment',
     'comments'				=> 'Comments',
     'postcomment'			=> 'Post Comment',
+    	'messages' 			=> array(
+					        'login'   		=> 'You need to be logged in to post comments.',
+					        'loginprompt'   => 'Click <a href=":signin">here</a> to login into your account.',
+					        'success' 		=> 'Your comment was successfully added.',
+					        'generic'		=> 'There was a problem adding your comment, please try again.',
+		),
 
 );
