8d07ff452a1857f1ba6553428f4c1fa231706f47
mapi <mauro.pizzamiglio@gmail.com>
2014-05-17 14:44:04 +0200

    database params

--- app/config/database.php
+++ app/config/database.php
@@ -55,8 +55,8 @@ return array(
         'mysql' => array(
             'driver'    => 'mysql',
             'host'      => 'localhost',
-            'database'  => 'laravel4-starter',
-            'username'  => 'root',
+            'database'  => 'laravel4_testing',
+            'username'  => 'travis',
             'password'  => '',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
